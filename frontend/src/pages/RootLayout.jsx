f4c4af96d5cdee71541f8e0a0b6807716088ada1
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-01-18 13:02:49 +0200

    using useNavigation to check active transition between routes

--- frontend/src/pages/RootLayout.jsx
+++ frontend/src/pages/RootLayout.jsx
@@ -1,10 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 const RootLayout = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       <MainNavigation />
+      {navigation.state === "loading" && (
+        <p style={{ textAlign: "center", fontSize: "32px", margin: "10px" }}>
+          Loading...
+        </p>
+      )}
       <main>
         <Outlet />
       </main>
