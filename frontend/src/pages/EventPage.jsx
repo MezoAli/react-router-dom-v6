6f9ebad68ca75c256d4e7c05460b5c95ce8ed107
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-01-18 18:01:46 +0200

    handle error by returning simpleobject

--- frontend/src/pages/EventPage.jsx
+++ frontend/src/pages/EventPage.jsx
@@ -3,16 +3,25 @@ import EventsList from "../components/EventsList";
 import { useLoaderData } from "react-router-dom";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  console.log(data);
+  const { events } = data;
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
   return <EventsList events={events} />;
 }
 
 export const getAllEventsLoader = async () => {
   try {
     const response = await axios.get("http://localhost:8080/events");
-    return response.data.events;
+    return response.data;
   } catch (error) {
-    console.log(error);
+    return {
+      isError: true,
+      message: "Can Not Fetch Events",
+      error,
+    };
   }
 };
 
